7617ce15aeff3ee7a6ebabcc74dfd2a36a81ccc7
Aaron <newcomer.aaron@icloud.com>
2016-05-24 10:26:41 -0400

    Add Broken Code

---
+++ 2016-05-24.js
@@ -0,0 +1,5 @@
+var numberOfChildren= "6";
+var partnersName ="Catelyn";
+var geoLoc ="Winterfell";
+var jobTitle="Warden of The North";
+console.log("You will be the " jobTitle " " "in" " " geoLoc ", " "and married to " partnersName "with" numberOfChildren " kids");
