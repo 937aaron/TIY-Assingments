b4eb27ff3f1f20172f2226f3561db266dd02c54d
Aaron <newcomer.aaron@icloud.com>
2016-05-24 10:32:18 -0400

    Add working code

--- 2016-05-24.js
+++ 2016-05-24.js
@@ -1,5 +1,13 @@
+// this is where I failed
+
+//var partnersName ="Catelyn";
+//var geoLoc ="Winterfell";
+//var jobTitle="Warden of The North";
+//console.log("You will be the " jobTitle " " "in" " " geoLoc ", " "and married to " partnersName "with" numberOfChildren " kids");
+
 var numberOfChildren= "6";
 var partnersName ="Catelyn";
 var geoLoc ="Winterfell";
 var jobTitle="Warden of The North";
-console.log("You will be the " jobTitle " " "in" " " geoLoc ", " "and married to " partnersName "with" numberOfChildren " kids");
+var fortune = "You will be a " + jobTitle + " in " + geoLoc + ", and married to " + partnersName + " with " + numberOfChildren + " kid.";
+console.log(fortune);
