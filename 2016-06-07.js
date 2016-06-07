3690677be95e37e820ebabe073175bfbae93ecea
Aaron <newcomer.aaron@icloud.com>
2016-06-07 10:48:54 -0400

    Add daily

---
+++ 2016-06-07.js
@@ -0,0 +1,10 @@
+var recipe = {title: "Lentil Soup", serving: "3", ingredients: ["Lentils", "Onions", "Salt", "Peper", "Chicken Stock"]};
+// var objRecipe = "";
+// for (property in recipe){
+// 	objRecipe= recipe[property];
+
+// }
+// console.log(objRecipe)
+recipe.getOwnPropertyNames(obj).forEach(function())){
+	console.log(val+ obj[val])
+}
