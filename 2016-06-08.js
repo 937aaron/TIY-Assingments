a0d83c8a07a0adbe75bd113c5b277c5c8bb4155e
Aaron <newcomer.aaron@icloud.com>
2016-06-08 10:21:13 -0400

    Added Daily

---
+++ 2016-06-08.js
@@ -0,0 +1,5 @@
+var faveMovie = {title: "OldBoy", Duration: 127, Stars: ["Min-sik Choi","Ji-tae Yu","Hye-jeong Kang"]};
+for (var i =0; i < faveMovie.Stars.length; i++);{
+console.log(faveMovie.title + " last for " + faveMovie.Duration + " minutes" + ". Stars " + faveMovie.Stars[0] + ", " + faveMovie.Stars[1] + ", " + faveMovie.Stars[2] )
+
+}
