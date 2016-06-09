35b1114d8059ac630db924825169b28b338400a9
Aaron <newcomer.aaron@icloud.com>
2016-06-09 10:27:10 -0400

    Add Daily

---
+++ 2016-06-09.js
@@ -0,0 +1,12 @@
+
+function drEvil (dollhairs) {
+	if (dollhairs < 1000000) {
+		console.log(dollhairs + " Dollars");
+	}
+	else {
+		console.log(dollhairs + " Dollars (pinky)");
+	}
+
+}
+drEvil(500000);
+drEvil(1000001);
